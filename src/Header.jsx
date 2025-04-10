3b65965b4041b83e621574d2f352e4ac1ce0afb2
George <gosha3484@gmail.com>
2025-04-10 20:26:26 +0300

    just test , added tailwind dependency

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Welcome to the Header</h1>
+            <Link to="/">Home</Link>
+        </header>
+    )
+
+
+}
