932ef24469d42e34e0cf0e3aba1d32b259f6c932
George <gosha3484@gmail.com>
2025-04-21 12:05:06 +0300

    button fixed

--- src/components/Home/BurgerSection.jsx
+++ src/components/Home/BurgerSection.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const BurgerSection = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <section className="burger-bg rounded-xl p-12 text-center text-white mb-12 animate-fadeIn">
                 <h2 className="text-4xl font-bold mb-4 text-shadow">Tervetuloa Burger Palaceen!</h2>
                 <p className="text-xl text-shadow">Maistuvimmat burgereit kaupungissa 🔥</p>
-                <button
+                <button onClick={() => navigate("/menu")}
                     className="mt-6 bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-8 rounded-full text-lg transition duration-300 transform hover:scale-105">
                     Tilaa nyt
                 </button>
