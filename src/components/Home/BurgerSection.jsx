7fa0ba50f6604c3c426aee03f15f1fdb0555fabe
George <gosha3484@gmail.com>
2025-04-30 20:49:28 +0300

    added dummy text

--- src/components/Home/BurgerSection.jsx
+++ src/components/Home/BurgerSection.jsx
@@ -1,14 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import RotatingBurger from "./RotatingBurger";
+import '../../styles/RotatingBurger.css';
 
 const BurgerSection = () => {
     const navigate = useNavigate();
 
     return (
-        <section className="burger-bg rounded-xl p-12 text-center text-white mb-12 animate-fadeIn">
+        <section className="burger-bg rounded-xl p-7 text-center text-white mb-12 animate-fadeIn">
             <RotatingBurger />
-            <h2 className="text-4xl font-bold mb-4 text-shadow">Tervetuloa Burger Palaceen!</h2>
-            <p className="text-xl text-shadow">Maistuvimmat burgereit kaupungissa 🔥</p>
+            <h2 className="text-4xl font-bold mb-6 text-shadow">Tervetuloa Burger Palaceen!</h2>
+
+
+
+            <div className="marquee-container mb-6">
+                <div className="marquee-content">
+                    <span className="marquee-text">
+                        MAISTUVIMMAT BURGERIT KAUPUNGISSA 🔥 SYÖ KUIN KUNINGAS – VAIN BURGER PALACEISSA! 👑&nbsp;
+                    </span>
+                    <span className="marquee-text">
+                        MAISTUVIMMAT BURGERIT KAUPUNGISSA 🔥 SYÖ KUIN KUNINGAS – VAIN BURGER PALACEISSA! 👑&nbsp;
+                    </span>
+                </div>
+            </div>
+
             <button
                 onClick={() => navigate("/menu")}
                 className="mt-6 bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-8 rounded-full text-lg transition duration-300 transform hover:scale-105"
